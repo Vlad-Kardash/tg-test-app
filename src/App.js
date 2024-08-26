ce706d5e2ab84c6e5d6d85d0419e5f37ff12b1b9
Vlad-Kardash <121450100+Vlad-Kardash@users.noreply.github.com>
2024-08-26 09:25:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
 const tg =window.Telegram.WebApp
 function App() {
+  const [postId, setPostId] = useState(null);
+
+  useEffect(() => {
+      
+      const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ "sign": "leo",
+    "language": "translated",
+    "period": "today" })
+      };
+      fetch('https://poker247tech.ru/get_horoscope/', requestOptions)
+          .then(response => response.json())
+          .then(data => setPostId(data.horoscope));
+          
+        }, []);
+        console.log(postId);
+
+
+
+  
   useEffect(()=>{
     tg.ready()
   },[])
@@ -13,10 +34,11 @@ function App() {
 
   return (
     <div className="App">
-     sdfsdf
+    {postId}
      <button onClick={onClose}>Закрыть</button>
     </div>
   );
 }
 
 export default App;
+
