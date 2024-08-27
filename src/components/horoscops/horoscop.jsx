c0808473ffecfc2931184cf89c6f73de042437e8
Vlad-Kardash <121450100+Vlad-Kardash@users.noreply.github.com>
2024-08-27 09:33:41 +0300

    fix

--- src/components/horoscops/horoscop.jsx
+++ src/components/horoscops/horoscop.jsx
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import ZODIAC from "../../constants/zodiacNames";
-import { initBackButton } from "@telegram-apps/sdk";
+// import { initBackButton } from "@telegram-apps/sdk";
 
 const AboutZodiac = () => {
-  const [backButton] = initBackButton();
-  backButton.show();
-  backButton.on("click", () => {
-    console.log("BackButton clicked.");
+  // const [backButton] = initBackButton();
+  // backButton.show();
+  // backButton.on("click", () => {
+  //   console.log("BackButton clicked.");
+  // });
+  var BackButton = WebApp.BackButton;
+  BackButton.show();
+  BackButton.onClick(function () {
+    WebApp.showAlert("BackButton clicked");
+    BackButton.hide();
   });
+  WebApp.onEvent("backButtonClicked", function () {});
   const params = useParams();
   const { i18n } = useTranslation();
   const [postId, setPostId] = useState([]);
