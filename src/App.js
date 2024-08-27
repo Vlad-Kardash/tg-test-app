4831bfcaa0a5a706a3abae330d4979eb170717b8
Vlad-Kardash <121450100+Vlad-Kardash@users.noreply.github.com>
2024-08-27 11:04:42 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import "./App.css";
 import URL_IMAGES from "./constants/urlImages";
 import ZODIAC from "./constants/zodiacNames";
 import { Link } from "react-router-dom";
-import { initBackButton } from "@telegram-apps/sdk";
 
 function App() {
-  const [backButton] = initBackButton();
-  backButton.show();
-  backButton.on("click", () => {
-    console.log("BackButton clicked.");
-  });
   return (
     <div className="">
       <div className="blockItem">
